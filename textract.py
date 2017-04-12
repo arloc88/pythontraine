6ad79006bb02e84c0e8fca2a95a7728198392782
arloc88 <riccio1988@gmail.com>
2017-04-12 12:28:37 +0200

    trova le frasi che cominciano con if e conta il numero di if trovati

--- textract.py
+++ textract.py
@@ -1,17 +1,29 @@
 from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
 import docx2txt
+import re
 
 def main():
 
-#prova = textract.process('prova.docx')
-# extract text
+    # extract text
     doc = docx2txt.process("prova.docx")
 
     blob = TextBlob(doc)
+    count=0
 
-    for s in blob.words:
-        print(s)
+    for s in blob.sentences:
+        if(s.lower().startswith('if')):
+            count += 1
+            print(" \n***** Nuovo If trovato: *****\n")
+            print(s)
 
+
+    print("\n Sono stati trovati " + str(count)+ " if " )
+
+
+    #print(doc)
+    #print ('+++++++++++++++++++++++++++++++++++++++++++++')
+    #print(blob)
 
 if __name__ == "__main__":
     main()
